464cd54dd1ee3985cf7d4ecfe5fa7ef1d6600e57
superhyuk <leogalcu@naver.com>
2022-02-06 01:41:38 +0900

    add checkeditor default value

--- makeBoard/boards1/models.py
+++ makeBoard/boards1/models.py
@@ -7,10 +7,12 @@ from django.contrib.auth.models import User
 
 #http 상태를 나타내기 위한 장고 모듈
 
-from ckeditor.fields import RichTextField
+from ckeditor.fields import RichTextField 
+#ckeditor 패키지 임포트
 
 class Topic(models.Model):
     message = models.TextField(max_length=5000,null=True)
+    wsyg = RichTextField(blank=True,null=True)# wysiwyg 추가
     subject = models.CharField(max_length=255)
     last_updated =  models.DateField(auto_now_add=True, null=True)
     writter = models.ForeignKey(User, related_name='topics',on_delete=models.CASCADE, null=True)    
